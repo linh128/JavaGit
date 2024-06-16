6ffc374ec88bb574c8b9fc3e9ae8ecf5beace182
linh128 <linhprofile@gmail.com>
2024-06-16 22:48:53 +0700

    10-48 Commit, test function
    test Person.hocsinh function

--- Lab4/src/lab4/bai2/Main.java
+++ Lab4/src/lab4/bai2/Main.java
@@ -4,6 +4,7 @@
  */
 package lab4.bai2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,24 +12,35 @@ import java.util.Scanner;
  * @author Administrator
  */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("==============MENU==============");
+        boolean flag = false;
         Scanner sc = new Scanner(System.in);
+        System.out.println("==============MENU==============");
         System.out.println("1 - Nhap va hien thong tin lop hoc");
         System.out.println("2 - Nhap va hien thong tin hoc sinh");
         System.out.println("3 - Nhap va hien thong tin giao vien");
         System.out.println("4 - Nhap va hien thong tin nhan vien");
-        System.out.println("Vui long chon chuc nang: ");
-        boolean flag = false;
-        try {
-            int n =  Integer.parseInt(sc.nextLine());
-            flag = true;
-        } catch (Exception e) {
-            System.out.println("LAB 4 ERROR!!! Msg: " +e);
+        while (flag == false) {
+            System.out.println("Vui long chon chuc nang: \n");
+            try {
+                int n = Integer.parseInt(sc.nextLine());
+                ArrayList a = new ArrayList();
+                if (n > 0 && n < 5 && n == 1) {
+                    flag = true;
+                    Person p = new HocSinh();
+                    p.nhapThongTin(sc);
+                } else {
+                    System.out.println("Lua chon khong hop le!!! \n");
+                }
+            } catch (Exception e) {
+                if (e.toString().contains("NumberFormatException")) {
+                    System.out.println("So nhap vao khong hop le!!!");
+                }else{
+                    System.out.println("LAB 4 ERROR!!! Msg: 10-47" + e);
+                }
+            }
         }
-        
-        HocSinh p = new HocSinh();
-        p.nhapThongTin(sc);
-        System.out.println(p.toString());
+
     }
 }
---
+++ Lab4/src/lab4/bai2/NhanVien.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4.bai2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author St
+ */
+public class NhanVien extends Person{
+    public String phongBan;
+    public String chucVu;
+
+    @Override
+    public void hienThiThongTin() {
+        System.out.println("Thong tin nhan vien: \n");
+        super.hienThiThongTin();
+    }
+
+    @Override
+    public void nhapThongTin(Scanner sc) {
+        super.nhapThongTin(sc);
+        System.out.println("Nhap phong ban: ");
+        this.phongBan = String.valueOf(sc.nextLine());
+        System.out.println("Nhap chuc vu: ");
+        this.chucVu = String.valueOf(sc.nextLine());
+    }
+    
+    
+
+    public NhanVien() {
+    }
+
+    public NhanVien(String phongBan, String chucVu, String name, String gender, String queQuan, int birthday) {
+        super(name, gender, queQuan, birthday);
+        this.phongBan = phongBan;
+        this.chucVu = chucVu;
+    }
+
+    public NhanVien(String phongBan, String chucVu) {
+        this.phongBan = phongBan;
+        this.chucVu = chucVu;
+    }
+
+    public String getPhongBan() {
+        return phongBan;
+    }
+
+    public void setPhongBan(String phongBan) {
+        this.phongBan = phongBan;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+    
+    
+}
